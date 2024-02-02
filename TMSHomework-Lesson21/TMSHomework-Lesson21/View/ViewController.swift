160989015a3e11cca21d884eadeb2d795fb702fc
Natallia Mazur <natashamazur@bk.ru>
2024-02-03 01:39:38 +0300

    Added underline as an attribute for second line of the attributed text of the label

--- TMSHomework-Lesson21/TMSHomework-Lesson21/View/ViewController.swift
+++ TMSHomework-Lesson21/TMSHomework-Lesson21/View/ViewController.swift
@@ -8,45 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     weak var label: UILabel!
     weak var attributesButton: UIButton!
     var attributedString: NSMutableAttributedString!
     var viewModel = LabelTextViewModel()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         label = makeLabel(size: viewModel.defaultText.textSize, text: viewModel.defaultText.string)
         setupAttributedString()
         attributesButton = makeButton(buttonTitle: "Add Attributes", action: #selector(addAttributesButtonTapped), otherElementBottomAnchor: label.bottomAnchor)
-        attributesButton = makeButton(buttonTitle: "Remove Attributes", action: #selector(removeAttributesuttonTapped), otherElementBottomAnchor: attributesButton.bottomAnchor)
+        attributesButton = makeButton(buttonTitle: "Remove Attributes", action: #selector(removeAttributesButtonTapped), otherElementBottomAnchor: attributesButton.bottomAnchor)
     }
-    
+
     private func setupDefautLabelText() {
         label.text = viewModel.defaultText.string
     }
-    
+
     private func setupAttributedString() {
         let labelText = viewModel.defaultText.string
         attributedString = NSMutableAttributedString(string: labelText)
     }
-    
+
     private func setupColorForWordInAttributedLabelText(color: UIColor, word: String, isBold: Bool) {
         let labelText = viewModel.defaultText.string
         let labelAttributes: [NSAttributedString.Key: Any] = [
             .foregroundColor: color,
         ]
         let range = findWordPositioninAttributedString(word: word, in: labelText)
-        
+
         attributedString.addAttributes(labelAttributes, range: range ?? NSMakeRange(0, 0))
-        
+
         if isBold {
             attributedString.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: viewModel.defaultText.textSize), range: range ?? NSMakeRange(0, 0))
         }
-        
+
         label.attributedText = attributedString
     }
     
+    private func setupUnderlineForLineInAttributedString(line: Int) {
+        let labelText = viewModel.defaultText.string
+        let range = findTextLineinAttributedString(in: labelText, line: line)
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: range ?? NSMakeRange(0, 0))
+        label.attributedText = attributedString
+    }
+
     private func findWordPositioninAttributedString(word: String, in text: String) -> NSRange? {
         if let wordRange = text.range(of: word) {
             let location = text.distance(from: text.startIndex, to: wordRange.lowerBound)
@@ -56,31 +62,50 @@ class ViewController: UIViewController {
         return nil
     }
     
+    private func findTextLineinAttributedString(in text: String, line: Int) -> NSRange? {
+        
+        if let text = label.text {
+            let lines = text.split(separator: "\n")
+            if lines.count >= line {
+                let linePosition = lines[line - 1]
+                
+                if let textRange = text.range(of: linePosition) {
+                    let location = text.distance(from: text.startIndex, to: textRange.lowerBound)
+                    let length = text.distance(from: textRange.lowerBound, to: textRange.upperBound)
+                    return NSMakeRange(location, length)
+                }
+            }
+        }
+        
+        return nil
+    }
+
     @objc func addAttributesButtonTapped() {
         setupColorForWordInAttributedLabelText(color: .blue, word: "Lorem", isBold: true)
         setupColorForWordInAttributedLabelText(color: .green, word: "ipsum", isBold: false)
+        setupColorForWordInAttributedLabelText(color: .red, word: "amet", isBold: false)
+        setupUnderlineForLineInAttributedString(line: 2)
     }
-    
-    @objc func removeAttributesuttonTapped() {
+
+    @objc func removeAttributesButtonTapped() {
         setupDefautLabelText()
     }
-
 }
 
-//ДЗ:
-//Создайте экран с использованием UIViewController. На этом экране должен быть UILabel для отображения текста. +
+// ДЗ:
+// Создайте экран с использованием UIViewController. На этом экране должен быть UILabel для отображения текста. +
 //
-//Используйте NSAttributedString для создания стилизованного текста в UILabel. +
+// Используйте NSAttributedString для создания стилизованного текста в UILabel. +
 //
-//Ваш текст должен включать следующие атрибуты:
+// Ваш текст должен включать следующие атрибуты:
 //
-//Разные цвета для различных слов. +
-//- Жирный и обычный шрифт для разных фраз. +
-//- Подчеркивание для одного из абзацев.
-//- Любые другие атрибуты, которые вы считаете интересными.
+// Разные цвета для различных слов. +
+// - Жирный и обычный шрифт для разных фраз. +
+// - Подчеркивание для одного из абзацев. +
+// - Любые другие атрибуты, которые вы считаете интересными.
 //
-//Реализуйте кнопку на экране. При нажатии на кнопку, текст в UILabel должен измениться, добавив новые атрибуты.
+// Реализуйте кнопку на экране. При нажатии на кнопку, текст в UILabel должен измениться, добавив новые атрибуты. +
 //
-//Добавьте еще одну кнопку для сброса текста в исходное состояние (без атрибутов).
+// Добавьте еще одну кнопку для сброса текста в исходное состояние (без атрибутов). +
 //
-//Добавьте возможность изменять атрибуты текста (цвет, стиль, размер шрифта) с использованием UISegmentedControl или других интерактивных элементов.
+// Добавьте возможность изменять атрибуты текста (цвет, стиль, размер шрифта) с использованием UISegmentedControl или других интерактивных элементов.
--- TMSHomework-Lesson21/TMSHomework-Lesson21/View/ViewControllerExtension.swift
+++ TMSHomework-Lesson21/TMSHomework-Lesson21/View/ViewControllerExtension.swift
@@ -8,42 +8,40 @@
 import UIKit
 
 extension ViewController {
-    
     func makeLabel(size: CGFloat, text: String) -> UILabel {
         let label = UILabel()
-        
+
         label.textColor = .black
         label.font = UIFont.systemFont(ofSize: size)
         label.numberOfLines = 3
         label.text = text
-        
+
         view.addSubview(label)
-        
+
         label.translatesAutoresizingMaskIntoConstraints = false
         label.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 40).isActive = true
         label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
+
         return label
     }
-    
+
     func makeButton(buttonTitle: String, action: Selector, otherElementBottomAnchor: NSLayoutYAxisAnchor) -> UIButton {
         let button = UIButton()
-        
+
         button.backgroundColor = UIColor.systemBlue
         button.layer.cornerRadius = 20
         button.setTitle(buttonTitle, for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.addTarget(self, action: action, for: .touchUpInside)
-        
+
         view.addSubview(button)
-        
+
         button.translatesAutoresizingMaskIntoConstraints = false
         button.topAnchor.constraint(equalToSystemSpacingBelow: otherElementBottomAnchor, multiplier: 10).isActive = true
         button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         button.widthAnchor.constraint(equalToConstant: 200).isActive = true
         button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
+
         return button
     }
-    
 }
